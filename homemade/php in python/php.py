650c074213189a63eb2b5ea59547a6905f20505a
uploads <85631165+21Turtles@users.noreply.github.com>
2022-09-16 17:06:02 -0400

    Update php.py

--- homemade/php in python/php.py
+++ homemade/php in python/php.py
@@ -3,6 +3,6 @@ import subprocess
 # if the script don't need output. ONLY EDIT THE path/to/your/script.php NOT THE "php  " at the end if you do it will result in failure
 subprocess.call("php /path/to/your/script.php")
 
-# if you want output
+# if you want output               SAME THING HERE
 proc = subprocess.Popen("php /path/to/your/script.php", shell=True, stdout=subprocess.PIPE)
 script_response = proc.stdout.read()
